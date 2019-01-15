6b58341ca51f415c00d693e8a7b2e2b923f14b10
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-01-15 20:20:37 +0000

    restructuring towards production

---
+++ rundevserver.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# load secret settings as environment variables
+# note . ./ below. This makes environment variables available in shell
+. ./.env
+
+# run django server with development server
+./manage.py runserver --settings=djangoproject.settings.development
