16629b5114a2f81a9424abfacca1530e8abca773
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-01-27 20:32:36 +0000

    work on permissions

--- lists/api.py
+++ lists/api.py
@@ -12,8 +12,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Instance must have an attribute named `created_by`.
-        return obj.created_by == request.user
+        if hasattr(obj, 'created_by'):
+            return obj.created_by == request.user
+
+        if hasattr(obj, 'list'):
+            if hasattr(obj.list, 'created_by'):
+                return obj.list.created_by == request.user
 
 
 class ListIsOwnerOrReadOnly(permissions.BasePermission):
@@ -47,7 +51,6 @@ class ListViewSet(viewsets.ModelViewSet):
     """
 
     permission_classes = [IsOwnerOrReadOnly]
-    # permission_classes = [permissions.AllowAny, ]
     model = List
     serializer_class = ListSerializer
 
@@ -60,26 +63,6 @@ class ListViewSet(viewsets.ModelViewSet):
             )
 
         return List.objects.filter(is_public=True)
-        # restrict any method that can alter a record
-        #restricted_methods = ['POST', 'PUT', 'PATCH', 'DELETE']
-        #if self.request.method in restricted_methods:
-            # if you are not logged in you cannot modify any list
-            #if not self.request.user.is_authenticated:
-              #return List.objects.none()
-
-            # you can only modify your own lists
-            # only a logged-in user can create a list and view the returned data
-            #return List.objects.filter(created_by=self.request.user)
-
-        # GET method (view list) is available to owner and for public lists
-        #if self.request.method == 'GET':
-          #if not self.request.user.is_authenticated:
-            #return List.objects.filter(is_public__exact=True)
-
-          #return List.objects.filter(Q(created_by=self.request.user) | Q(is_public__exact=True))
-
-        # explicitly refuse any non-handled methods
-        #return List.objects.none()
 
     def pre_save(self, obj):
         obj.created_by = self.request.user
@@ -87,15 +70,12 @@ class ListViewSet(viewsets.ModelViewSet):
 
 class ListBySlugViewSet(viewsets.ModelViewSet):
     """
-    Find a list by slug. The list is only returned if the user is authorized to see it.
-
-    Only get, head are permitted.
+    Find a list by slug.
     """
-    #permission_classes = [IsOwnerOrReadOnly]
-    permission_classes = [permissions.AllowAny, ]
+    permission_classes = [IsOwnerOrReadOnly]
     model = List
     serializer_class = ListSerializer
-    #http_method_names = ['get', 'head']
+
     def get_queryset(self):
         # can view public lists and lists the user created
         if self.request.user.is_authenticated:
@@ -106,18 +86,14 @@ class ListBySlugViewSet(viewsets.ModelViewSet):
 
         return List.objects.filter(slug=self.request.query_params.get('slug', None)).filter(is_public=True)
 
-    #def get_queryset(self):
-        #if not self.request.user.is_authenticated:
-            #return List.objects.filter(slug__exact=self.request.query_params.get('slug', None)).filter(is_public__exact=True)
-
-        #return List.objects.filter(slug__exact=self.request.query_params.get('slug', None)).filter(Q(created_by=self.request.user) | Q(is_public__exact=True))
 
 class ItemViewSet(viewsets.ModelViewSet):
-    permission_classes = [ListIsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     model = Item
     serializer_class = ItemSerializer
 
     def get_queryset(self):
+        # can view items belonging to public lists and lists the usesr created
         if self.request.user.is_authenticated:
             return Item.objects.filter(
                 Q(list__created_by=self.request.user) | 
@@ -125,33 +101,4 @@ class ItemViewSet(viewsets.ModelViewSet):
             )
 
         return Item.objects.filter(list__is_public=True)
-
-
-    #permission_classes = [permissions.AllowAny, ]
-    #model = Item
-    #serializer_class = ItemSerializer
-
-    #def get_queryset(self):
-        # restrict any method that can alter a record
-        #restricted_methods = ['POST', 'PUT', 'PATCH', 'DELETE']
-        #if self.request.method in restricted_methods:
-            # if you are not logged in you cannot modify any list
-            #if not self.request.user.is_authenticated:
-              #return Item.objects.none()
-
-            # you can only modify your own lists
-            # only a logged-in user can create a list and view the returned data
-            # return Item.objects.filter(created_by=self.request.user)
-            #return Item.objects.all()
-
-        # GET method (view item) is available to owner and for items in public lists
-        #if self.request.method == 'GET':
-          #if not self.request.user.is_authenticated:
-            #return Item.objects.filter(is_public__exact=True)
-            #return Item.objects.all()
-
-          #return Item.objects.filter(Q(created_by=self.request.user) | Q(is_public__exact=True))
-
-        # explicitly refuse any non-handled methods
-        #return Item.objects.none()
 
