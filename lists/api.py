6f84475203894b7b0b457dc27db85181f34e8b48
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-01-28 16:21:35 +0000

    Work on list and item permissions

--- lists/api.py
+++ lists/api.py
@@ -20,36 +20,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
                 return obj.list.created_by == request.user
 
 
-class ListIsOwnerOrReadOnly(permissions.BasePermission):
-    """Note typos in Stackoverflow post
-    def get_queryset(self):
-        if self.request.user.is_authenticated
-
-    needs colon after is_authenticated
-
-    permission_classes = [IsOwnerOrReadyOnly]
-    should be ReadOnly not ReadyOnly
-
-    why list__is_public__exact in first line of queryset, but list__is_public in second?
-
-    https://stackoverflow.com/questions/9963200/in-django-filter-statement-whats-the-difference-between-exact-and-equal-sign
-    """
-    def has_object_permission(self, request, view, obj):
-        # handle permissions based on method
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance's parent list must have an attribute named `created_by`.
-        return obj.list.created_by == request.user
-
-
 class ListViewSet(viewsets.ModelViewSet):
     """
     ViewSet for lists.
     """
-
     permission_classes = [IsOwnerOrReadOnly]
     model = List
     serializer_class = ListSerializer
@@ -97,7 +71,7 @@ class ItemViewSet(viewsets.ModelViewSet):
         if self.request.user.is_authenticated:
             return Item.objects.filter(
                 Q(list__created_by=self.request.user) | 
-                Q(list__is_public__exact=True)
+                Q(list__is_public=True)
             )
 
         return Item.objects.filter(list__is_public=True)
