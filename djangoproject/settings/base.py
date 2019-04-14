5832d205146a20ce9f6ebf43adf2cefb022da643
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-04-14 18:08:44 +0100

    experiment with static

--- djangoproject/settings/base.py
+++ djangoproject/settings/base.py
@@ -143,11 +143,11 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets/static'),
-    os.path.join(BASE_DIR, 'assets'),
+    # os.path.join(BASE_DIR, 'assets'),
 ]
 
 # used by admin site
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Django rest framework
 REST_FRAMEWORK = {
