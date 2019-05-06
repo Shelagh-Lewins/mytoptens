178a5bef321d7d7f39501e63da5f7ab6c29fbd88
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-05-06 14:30:11 +0100

    test build for deployment

---
+++ static/precache-manifest.986ee7eecc5cfd936c5c3deba17704fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e918e68de24e6ce48682db8460f42f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/css/2.fe0551f8.chunk.css"
+  },
+  {
+    "revision": "0f85e8b5bf423de99517",
+    "url": "/static/css/main.235fbfc2.chunk.css"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/js/2.b90444c1.chunk.js"
+  },
+  {
+    "revision": "0f85e8b5bf423de99517",
+    "url": "/static/js/main.4ae7610a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7365d01f82ef97873b55d5f1deb4c88",
+    "url": "/static/media/private.d7365d01.svg"
+  },
+  {
+    "revision": "20e4a36ef90936c2d02d0e48f524fdb5",
+    "url": "/static/media/public.20e4a36e.svg"
+  }
+]);
---
+++ static/precache-manifest.f8b8e902d97dc68232f5b40e68c4a698.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873ea1e10aeff2987e79531d2724fa52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/css/2.fe0551f8.chunk.css"
+  },
+  {
+    "revision": "663bf75716c9ca31d101",
+    "url": "/static/css/main.235fbfc2.chunk.css"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/js/2.b90444c1.chunk.js"
+  },
+  {
+    "revision": "663bf75716c9ca31d101",
+    "url": "/static/js/main.3dc43196.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7365d01f82ef97873b55d5f1deb4c88",
+    "url": "/static/media/private.d7365d01.svg"
+  },
+  {
+    "revision": "20e4a36ef90936c2d02d0e48f524fdb5",
+    "url": "/static/media/public.20e4a36e.svg"
+  }
+]);
