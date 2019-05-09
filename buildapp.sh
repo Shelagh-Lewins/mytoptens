d07dacb84a3c084cc9b23e741d899eb480723ac0
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-05-09 18:03:41 +0100

    Updates to scripts for build and deployment

--- buildapp.sh
+++ buildapp.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+# load secret settings as environment variables
+# note . ./ below. This makes environment variables available in shell
+. .env
+
+# build the frontend
 npm run build --prefix frontend
 rm -rf ./assets
 mv ./frontend/build ./assets
 
-. .env
+
+# collect static files to production location
 yes yes | ./manage.py collectstatic --settings=djangoproject.settings.development
 
+# record python dependencies
 pip freeze > requirements.txt
+
+# make and apply database migrations
+./migrations.sh
--- deploy/initiate.sh
+++ deploy/initiate.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+set -e
+
+# use to reference other scripts in the same directory as this one
+my_dir=`dirname $0`
+
+# load environment variables: SERVER
+. $my_dir/.env
 
 ### Configuration ###
 SOURCE=$HOME"/Web tutorials/mytoptens/mytoptens"
-DESTINATION="mytoptens@178.62.85.245:~/"
 
+echo $DESTINATION
 # build app
 echo "building app..."
 ./buildapp.sh
@@ -11,14 +18,12 @@ echo "building app..."
 # remove old app update files from the server
 # this forces a complete refresh of all files instead of relying on rsync to detect changes
 echo "deleting old update files from server..."
-ssh mytoptens@178.62.85.245 rm -f -r mytoptens
+ssh $DESTINATION rm -f -r mytoptens
 
 # copy new app update files onto the server
 echo "copying new update files onto server..."
-rsync -arv  --filter=":- .gitignore" "$SOURCE" "$DESTINATION"
+rsync -arv  --filter=":- .gitignore" "$SOURCE" "$DESTINATION"":~/"
 
 # run the deployment script on the server
-# allow the script to be found even if you're not running initiate.sh from the same directory
 echo "deploying app on server..."
-my_dir=`dirname $0`
-ssh mytoptens@178.62.85.245 'bash -s' < $my_dir/serverscript.sh
+ssh  $DESTINATION 'bash -s' < $my_dir/serverscript.sh
