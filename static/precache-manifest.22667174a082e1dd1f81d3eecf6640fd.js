b3a9f7ebba2b06739e801c896683481a30978d60
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-05-13 17:33:37 +0100

    test build

---
+++ static/precache-manifest.22667174a082e1dd1f81d3eecf6640fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718522345ddcadf8f3ebb674a6e3c16d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/css/2.fe0551f8.chunk.css"
+  },
+  {
+    "revision": "a6ed5ff5c346c1d6a355",
+    "url": "/static/css/main.2e00ff94.chunk.css"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/js/2.b90444c1.chunk.js"
+  },
+  {
+    "revision": "a6ed5ff5c346c1d6a355",
+    "url": "/static/js/main.1bea649a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7365d01f82ef97873b55d5f1deb4c88",
+    "url": "/static/media/private.d7365d01.svg"
+  },
+  {
+    "revision": "20e4a36ef90936c2d02d0e48f524fdb5",
+    "url": "/static/media/public.20e4a36e.svg"
+  }
+]);
---
+++ static/precache-manifest.53631ee6657b9f6a5523bc18aa6c6e0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978324d7f37ed0dc9c52f103fc11d99f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/css/2.fe0551f8.chunk.css"
+  },
+  {
+    "revision": "745a7b2cd9b57f2f32f6",
+    "url": "/static/css/main.cbf9cab0.chunk.css"
+  },
+  {
+    "revision": "2f6c1dd865966c18022c",
+    "url": "/static/js/2.b90444c1.chunk.js"
+  },
+  {
+    "revision": "745a7b2cd9b57f2f32f6",
+    "url": "/static/js/main.1bea649a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7365d01f82ef97873b55d5f1deb4c88",
+    "url": "/static/media/private.d7365d01.svg"
+  },
+  {
+    "revision": "20e4a36ef90936c2d02d0e48f524fdb5",
+    "url": "/static/media/public.20e4a36e.svg"
+  }
+]);
