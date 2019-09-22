15d7c1c1f8d11c9dad30403de2fd8c86cbce5104
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-09-22 17:00:21 +0100

    Removed some comments

--- frontend/src/modules/topTenList.js
+++ frontend/src/modules/topTenList.js
@@ -505,10 +505,7 @@ export default function topTenList(state = initialTopTenListsState, action) {
 				'count': count,
 				'previous': previous,
 				'next': next,
-				// 'things': updeep.constant(things), // constant provides placement instead of update, so all previous entries are removed
 				'things': things, // add data because notifications also use top ten lists
-				// 'organizerData': updeep.constant({}), // new topTenList data so clear out old organizer data, this must be loaded separately
-				// update: must not clear out old organizer data, because notifications contain top ten list information
 				'isLoading': false,
 			}, state);
 		}
